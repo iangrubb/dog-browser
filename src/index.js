41d96cb560a9a41f193a6d838c5e938f14b79ac9
IanGrubb <hi@iangrubb.com>
2020-11-11 14:04:41 -0500

    Adds the ability to fetch new shiba images

--- src/index.js
+++ src/index.js
@@ -2,15 +2,26 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
     const mainTag = document.querySelector("main")
 
-    console.log(mainTag)
-
     const renderShiba = shibaUrl => {
-        mainTag.innerHTML = `<img src="${shibaUrl}" alt="A Shiba" width="200" >`
+        mainTag.innerHTML = `
+            <button id="new-random-shiba">New Shiba</button>
+            <img src="${shibaUrl}" alt="A Shiba" width="200" >
+            `
     }
 
-    fetch("https://dog.ceo/api/breed/shiba/images/random")
-    .then(resp => resp.json())
-    .then(data => renderShiba(data.message))
+    const fetchShibaImage = () => {
+        return fetch("https://dog.ceo/api/breed/shiba/images/random")
+        .then(resp => resp.json())
+    }
+
+    fetchShibaImage().then(data => renderShiba(data.message))
+
+    mainTag.addEventListener("click", e => {
+        console.log(e.target.id)
+        if (e.target.id === "new-random-shiba") {
+            fetchShibaImage().then(data => renderShiba(data.message))
+        }
+    })
 
 
 
