1907b8b96ca0b64585d6b2b4bafa263b50100ea0
IanGrubb <hi@iangrubb.com>
2020-11-11 15:02:44 -0500

    Adds breed fetching, parsing, and a dropdown in navbar

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
     const mainTag = document.querySelector("main")
     const headerTag = document.querySelector("header")
+    const navTag = document.querySelector("nav")
 
     // Shiba images (set in case of fetching the same random image again)
 
@@ -11,6 +12,32 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
     const saveShiba = shibaUrl => {
         viewedShibas.add(shibaUrl)
+    }
+
+
+    // Breed logic
+
+    const parseBreeds = resp => {
+        const breeds = []
+
+        for (const breed in resp){
+            const subBreeds = resp[breed]
+            if (subBreeds.length === 0) {
+                breeds.push({name: breed, stub: breed})
+            } else {
+                subBreeds.forEach(subBreed => {
+                    breeds.push({name: `${subBreed} ${breed}`, stub: `${breed}/${subBreed}`})
+                })
+            }
+        }
+
+        return breeds
+    }
+
+    const fetchBreeds = () => {
+        return fetch("https://dog.ceo/api/breeds/list/all")
+        .then(resp => resp.json())
+        .then(data => parseBreeds(data.message))
     }
 
 
@@ -47,23 +74,53 @@ document.addEventListener("DOMContentLoaded", ()=>{
         .join("")
     }
 
+    // Navigation for a breed
 
-    // DOM manipulation
+    const breedOptionHTML = (breed, isSelected) => `<option ${isSelected ? "selected" : ""}>${breed.name}</option>`
 
-    loadRandomShibaView()
+    const loadNavigationForBreed = (breeds, selectedBreed) => {
+        navTag.innerHTML= `
+            <span>Select Breed:<span>
+            <select>
+                ${breeds.map(breed => breedOptionHTML(breed, breed.name === selectedBreed.name)).join("")}
+            <select>
+            <button id="random-shiba-button">Random ${selectedBreed.name}</button>
+            <button id="viewed-shibas-button">Viewed ${selectedBreed.name}s</button>
+        `
+    }
 
-    mainTag.addEventListener("click", e => {
-        
 
-    })
+    // App initialization and first render
 
-    headerTag.addEventListener("click", e => {
-        if (e.target.id === "random-shiba-button") {
-            loadRandomShibaView()
-        } else if (e.target.id === "viewed-shibas-button") {
-            loadViewedShibasView()
-        }
-    })
+    const renderApp = breeds => {
+
+        console.log(breeds)
+
+        loadNavigationForBreed(breeds, {name: "shiba", stub: "shiba"})
+
+        loadRandomShibaView()
+
+        mainTag.addEventListener("click", e => {
+            
+
+        })
+
+        headerTag.addEventListener("click", e => {
+            if (e.target.id === "random-shiba-button") {
+                loadRandomShibaView()
+            } else if (e.target.id === "viewed-shibas-button") {
+                loadViewedShibasView()
+            }
+        })
+
+        navTag.addEventListener("change", e => {
+            console.log("Selected Name:", e.target.value)
+        })
+
+    }
+
+    fetchBreeds()
+    .then(renderApp)
 
 
 
