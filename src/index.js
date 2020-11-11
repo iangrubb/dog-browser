bd0e18d0078fa053670d07d7ccf239efaabaf7b8
IanGrubb <hi@iangrubb.com>
2020-11-11 13:56:58 -0500

    Adds fetch for a random shiba and renders it

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 document.addEventListener("DOMContentLoaded", ()=>{
 
-    
+    const mainTag = document.querySelector("main")
+
+    console.log(mainTag)
+
+    const renderShiba = shibaUrl => {
+        mainTag.innerHTML = `<img src="${shibaUrl}" alt="A Shiba" width="200" >`
+    }
+
+    fetch("https://dog.ceo/api/breed/shiba/images/random")
+    .then(resp => resp.json())
+    .then(data => renderShiba(data.message))
+
+
 
 })
 
